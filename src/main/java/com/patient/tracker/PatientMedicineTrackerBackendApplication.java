8b84a463254c2c4811116e8a4b93b0ee2ea25da0
Hasshya Krishnamoorthy <71715036+hasshya1530@users.noreply.github.com>
2025-07-03 15:45:27 +0530

    Initial backend commit

---
+++ src/main/java/com/patient/tracker/PatientMedicineTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.patient.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMedicineTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientMedicineTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patient/tracker/PatientMedicineTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patient.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientMedicineTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
